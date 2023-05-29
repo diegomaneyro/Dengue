1a7790823f6ee002a80f2e3c7d371fd491374534
Diego <diegomaneyro@gmail.com>
2023-05-29 15:32:41 -0300

    Update analisis.py

--- streamlit/analisis.py
+++ streamlit/analisis.py
@@ -11,7 +11,7 @@ def pagina_analisis():
     """)
     
     #cargar datos desde csv
-    ruta_datos = "datos/datosOriginales/dengue-zika-2022.csv"
+    ruta_datos ="datos/limpieza/eda/dengue-zika-clean.csv" 
     
     df = pd.read_csv(ruta_datos, encoding='latin-1', sep=";")  
 
