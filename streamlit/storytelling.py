b1ff675b2100e3b8e1a81dfeccad2a89ae97485c
Diego <diegomaneyro@gmail.com>
2023-05-29 15:36:40 -0300

    Update storytelling.py

--- streamlit/storytelling.py
+++ streamlit/storytelling.py
@@ -4,11 +4,7 @@ def pagina_storytelling():
     st.markdown("### Introduccion al proyecto ")
 
     #ruta local del video
-<<<<<<< HEAD
     video_path = 'storytelling.mp4'
-=======
-    video_path = 'storytelling.mp4'
->>>>>>> f49fb53b3760ac7f51c1822135360bf9d952da90
 
     #mostrar video
     st.video(video_path)
