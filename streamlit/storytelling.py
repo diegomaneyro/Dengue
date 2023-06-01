1e43112d419ae1c8b5b0c4bfd7dbb640aa0fce14
Diego <diegomaneyro@gmail.com>
2023-06-01 13:13:06 -0300

    Update storytelling.py

--- streamlit/storytelling.py
+++ streamlit/storytelling.py
@@ -6,7 +6,7 @@ def pagina_storytelling():
     st.markdown("---")
     #ruta local del video
 
-   video_path = 'https://drive.google.com/file/d/1_l-2gXw7g32gR2OnOsz19JuxKpOGRH5u/view?usp=drive_link'
+    video_path = 'https://drive.google.com/file/d/1_l-2gXw7g32gR2OnOsz19JuxKpOGRH5u/view?usp=drive_link'
 
 
     #mostrar video
