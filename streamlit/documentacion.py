1c29f620b653a073756cbfe33138225ab48d1049
Diego <diegomaneyro@gmail.com>
2023-06-02 10:58:45 -0300

    Update documentacion.py

--- streamlit/documentacion.py
+++ streamlit/documentacion.py
@@ -7,25 +7,24 @@ def pagina_documentacion():
     
     st.write("""
 
-El proyecto "Dengue y Zika en Argentina" tiene como objetivo recopilar, analizar los casos de dengue y zika registrados en Argentina.
+El proyecto "Dengue en Argentina" tiene como objetivo recopilar, analizar los casos de dengue registrados en Argentina.
 Utilizando fuentes de datos confiables proporcionadas por
 el Gobierno de la Nacion Argentina desde la web Oficial www.datos.gob.ar. Este proyecto busca brindar información actualizada y detallada. 
 """)
     st.markdown("#### Objetivos")
     st.write("""
-Recopilar y procesar los datos oficiales de los casos de dengue y zika en Argentina.
+Recopilar y procesar los datos oficiales de los casos de dengue en Argentina.
 Realizar un análisis epidemiológico de la evolución temporal de los casos.
-Identificar las regiones y provincias más afectadas por el dengue y zika.
+Identificar las regiones y provincias más afectadas por el dengue.
 Visualizar los datos de manera clara y accesible mediante gráficos y mapas interactivos.
 Proporcionar información útil para los profesionales de la salud y el público en general.
 """)
     st.markdown("#### Herramientas Utilizadas")
     st.write("""
 
-* Lenguaje de programación: Python 3, sql
-* Bibliotecas: Pandas, Matplotlib, Seaborn, Sklearn 
+* Lenguaje de programación: Python
+* Bibliotecas: Pandas, Matplotlib, Seaborn 
 * Frameworks: Streamlit: www.share.streamlit.io
-* Base de datos: MySQL: www.console.clever-cloud.com
 * Herramienta de visualización: Visual Studio Code, Jupyter Notebook
 * Repositorio: Github: www.github.com
 * Control de versiones: Git
@@ -47,20 +46,18 @@ Análisis de la evolución temporal de los casos.
 
 * Visualización de datos:
 Creación de gráficos y visualizaciones interactivas para representar los datos de manera clara.
-Generación de mapas temáticos que muestren la distribución geográfica de los casos.
 
 * Documentación y presentación:
 Documentación detallada de los pasos realizados, metodologías y resultados obtenidos.
 Preparación de informes y presentaciones visuales para compartir los hallazgos del proyecto.
-Resultados Esperados
+Resultados Esperados.
 
 """)
     st.markdown("#### Al finalizar el proyecto, se espera obtener")
     st.write("""
 
-Un análisis completo de los casos de dengue y zika en Argentina, con una descripción detallada.
-Gráficos y visualizaciones interactivas que permitan explorar y comprender los datos de manera intuitiva 
-mapas temáticos que muestren la distribución geográfica de los casos en diferentes regiones y provincias,
+Un análisis completo de los casos de dengue en Argentina en 2022, con una descripción detallada.
+Gráficos y visualizaciones interactivas que permitan explorar y comprender los datos de manera intuitiva de los casos en diferentes regiones y provincias,
 información útil y actualizada sobre la incidencia de estas enfermedades para los profesionales de la salud y publico en gral.
 
 Consideraciones Éticas y de Privacidad
@@ -73,39 +70,22 @@ Se seguirán todas las normativas y mejores prácticas en la gestión de datos s
     
     #Crear columnas
     col1, col2 = st.columns(2)
-
-    #Agregar elementos a las columnas
+    
     with col1:        
-        st.write("""Aquí pueden descargar este archivo en formato pdf
-        """)
         
-        # Ruta del archivo PDF que deseas descargar
-        ruta_pdf = "documentacion/documentacion.pdf"
-
-        # Botón de descarga del archivo PDF
-        with open(ruta_pdf, "rb") as file:
-            btn = st.download_button(
-                label="Download documentacion",
-                data=file,
-                file_name="documentacion.pdf",
-                mime="application/pdf"
-            )
-    with col2:        
         st.markdown("#### Repositorio")
         st.write("""
         Aquí encontraraa el link al repositorio de este proyecto
         """)
-        # Ruta del archivo PDF que deseas descargar
-        ruta_pdf = "documentacion/documentacion.pdf"
-
         
         # URL del repositorio
         repo_url = "https://github.com/diegomaneyro/DengueZikaArgentina"
     
         # Mostrar el enlace al repositorio
         st.markdown(f"[Enlace al repositorio]({repo_url})")
-
+               
         
+    with col2:        
         
         st.markdown("#### Licencia")
         
